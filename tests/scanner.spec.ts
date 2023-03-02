b91d3f81fe37746061db8372cfb51e0b735d6f24
Theo Sun <theo.sun@outlook.com>
2023-03-02 19:45:21 +0800

    feat: better test assert

--- tests/scanner.spec.ts
+++ tests/scanner.spec.ts
@@ -1,6 +1,7 @@
 import { scanDirectoryWithResult } from "../src/scanner";
 import { join } from "path";
 import { testfile1AbsPath, testfile3AbsPath, testfile4AbsPath, testfile5AbsPath, testfile6AbsPath } from "./test.base";
+import { mapScanResultToReportVO } from "../src/file";
 
 describe('scanner related test', () => {
 
@@ -22,5 +23,13 @@ describe('scanner related test', () => {
       testfile6AbsPath,
     ])
   })
+
+  it('should support scan all projects', () => {
+    for (const proj of ['', 2, 3, 4, 5, 6, 7, 8]) {
+      const projName = `testproject${proj}`
+      const r = mapScanResultToReportVO(scanDirectoryWithResult(join(__dirname, `./${projName}`)))
+      expect(r).toMatchSnapshot(`report for project ${projName}`)
+    }
+  });
 
 })
